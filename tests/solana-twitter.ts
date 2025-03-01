e7d50bfac8797a51f89bc1ef8f4843cebcc84687
explorer-Sanjita <sanjita2908@gmail.com>
2025-03-01 15:45:12 +0530

    Test script with 5 test scenerios written in solana-twitter.ts file

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,16 +1,143 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
+import * as assert from "assert";
 
 describe("solana-twitter", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+    const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    it('can send a new tweet', async () => {
+        // Before sending the transaction to the blockchain.
+        const tweet = anchor.web3.Keypair.generate();
+        await program.rpc.sendTweet('SheFi', 'I am a SheFi Scholar', {
+            accounts: {
+                // Accounts here...
+                tweet: tweet.publicKey,
+                author: provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [tweet],
+            // Key pairs of signers here...,
+        });
+
+        // After sending the transaction to the blockchain.
+
+        //Fetch account details of created tweet account
+        const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+        //console.log(tweetAccount);
+
+        // Ensure it has the right data.
+        assert.equal(tweetAccount.author.toBase58(), provider.wallet.publicKey.toBase58());
+        assert.equal(tweetAccount.topic, 'SheFi');
+        assert.equal(tweetAccount.content, 'I am a SheFi Scholar');
+        assert.ok(tweetAccount.timestamp);
+    });
+
+    // test scenerio 2
+
+    it('can send a new tweet without a topic', async () => {
+        // Call the "SendTweet" instruction.
+        const tweet = anchor.web3.Keypair.generate();
+        await program.rpc.sendTweet('', 'gm', {
+            accounts: {
+                tweet: tweet.publicKey,
+                author: provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [tweet],
+        });
+
+        // Fetch the account details of the created tweet.
+        const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+        // Ensure it has the right data.
+        assert.equal(tweetAccount.author.toBase58(), provider.wallet.publicKey.toBase58());
+        assert.equal(tweetAccount.topic, '');
+        assert.equal(tweetAccount.content, 'gm');
+        assert.ok(tweetAccount.timestamp);
+    });
+
+    // test scenerio 3 : where we generate a new key pair & sign transation using it instaed of using our wallet
+    // this gives an error beacause i have airdropped sol to wallet that i am using right now 
+    // but this newly generated wallet doesn't have any lamports
+
+    it('can send a new tweet from a different author', async () => {
+        // Generate another user and airdrop them some SOL.
+        const otherUser = anchor.web3.Keypair.generate();
+        const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+        const latestBlockhash = await program.provider.connection.getLatestBlockhash();
+
+        await program.provider.connection.confirmTransaction({
+            signature,
+            blockhash: latestBlockhash.blockhash,
+            lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+        }, 'finalized');
+
+
+        // Call the "SendTweet" instruction on behalf of this other user.
+        const tweet = anchor.web3.Keypair.generate();
+        await program.rpc.sendTweet('SheFi', 'Web3\'s future is feminine!', {
+            accounts: {
+                tweet: tweet.publicKey,
+                author: otherUser.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [otherUser, tweet],
+        });
+
+        // Fetch the account details of the created tweet.
+        const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+        // Ensure it has the right data.
+        assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+        assert.equal(tweetAccount.topic, 'SheFi');
+        assert.equal(tweetAccount.content, 'Web3\'s future is feminine!');
+        assert.ok(tweetAccount.timestamp);
+    });
+
+    // test scenerio 4
+    it('cannot provide a topic with more than 50 characters', async () => {
+        try {
+            const tweet = anchor.web3.Keypair.generate();
+            const topicWith51Chars = 'x'.repeat(51);
+            await program.rpc.sendTweet(topicWith51Chars, 'Hummus, am I right?', {
+                accounts: {
+                    tweet: tweet.publicKey,
+                    author: program.provider.wallet.publicKey,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                },
+                signers: [tweet],
+            });
+        } catch (error) {
+            assert.equal(error.msg, 'The provided topic should be 50 characters long maximum.');
+            return;
+        }
+    
+        assert.fail('The instruction should have failed with a 51-character topic.');
+    });
+
+    // test scenerio 5
+    it('cannot provide a content with more than 280 characters', async () => {
+        try {
+            const tweet = anchor.web3.Keypair.generate();
+            const contentWith281Chars = 'x'.repeat(281);
+            await program.rpc.sendTweet('veganism', contentWith281Chars, {
+                accounts: {
+                    tweet: tweet.publicKey,
+                    author: program.provider.wallet.publicKey,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                },
+                signers: [tweet],
+            });
+        } catch (error) {
+            assert.equal(error.msg, 'The provided content should be 280 characters long maximum.');
+            return;
+        }
+    
+        assert.fail('The instruction should have failed with a 281-character content.');
+    });
+
 });
